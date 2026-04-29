f83253a98ac8ea5dc1ae62987e11b649f24b0d70
Wayne <gzhw734@gmail.com>
2026-04-29 16:25:49 +0800

    core: add exception hierarchy (AgentflowError + subtypes)
    Adds header-only AgentflowError base and derived types (GraphCompileError,
    LlmError, LlmAbortedError, LlmOomError, ToolError, McpTransportError).
    Wires agentflow_core CMake target and 3-test GTest suite; all tests pass.

---
+++ agentflow/core/errors.cc
@@ -0,0 +1,4 @@
+// agentflow/core/errors.cc
+#include "agentflow/core/errors.h"
+// Intentionally empty: all classes are defined inline in the header.
+namespace agentflow {}  // namespace agentflow
