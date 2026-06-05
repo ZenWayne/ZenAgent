4fd5c68d122b743d18d837a12142d1d6015b4b83
Wayne <gzhw734@gmail.com>
2026-06-06 02:20:46 +0800

    feat(p11): JSONPath subset + EvalContext for workflow templates

---
+++ agentflow/workflow/json_path.h
@@ -0,0 +1,27 @@
+#ifndef AGENTFLOW_WORKFLOW_JSON_PATH_H_
+#define AGENTFLOW_WORKFLOW_JSON_PATH_H_
+
+#include <optional>
+#include <string>
+#include <string_view>
+#include <variant>
+#include <vector>
+
+#include <absl/status/statusor.h>
+#include <nlohmann/json.hpp>
+
+namespace agentflow::workflow {
+
+// JSONPath subset: $.field[N].field. No filters, no wildcards, no recursive descent.
+class JsonPath {
+ public:
+  static absl::StatusOr<JsonPath> Parse(std::string_view expr);
+  std::optional<nlohmann::ordered_json> Resolve(const nlohmann::ordered_json& root) const;
+ private:
+  using Segment = std::variant<std::string, int>;
+  std::vector<Segment> segments_;
+};
+
+}  // namespace agentflow::workflow
+
+#endif  // AGENTFLOW_WORKFLOW_JSON_PATH_H_
